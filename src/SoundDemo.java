96762faf43f1d5d0775a402fd5be5737019e7835
Derek Jamison <derekj@microsoft.com>
2016-10-11 20:13:46 -0700

    Refactored main method into JSMusic class

--- src/SoundDemo.java
+++ src/SoundDemo.java
@@ -3,41 +3,12 @@ import javax.sound.midi.*;
 public class SoundDemo {
 
 	public static void main(String[] args) throws Exception {
-		Sequence sequence = new Sequence(Sequence.PPQ, 16);
-		
-		Track track = sequence.createTrack();
-		
-		ShortMessage shortMessage = new ShortMessage();
-		shortMessage.setMessage(ShortMessage.PROGRAM_CHANGE, 0, 0, 0);
-		MidiEvent event = new MidiEvent(shortMessage, 0); 
-		track.add(event);
-		
-		ShortMessage onShortMessage = new ShortMessage();
-		onShortMessage.setMessage(ShortMessage.NOTE_ON, 0, 60, 64);
-		MidiEvent eventOn = new MidiEvent(onShortMessage, 70);
-		track.add(eventOn);
-
-		ShortMessage offShortMessage = new ShortMessage();
-		offShortMessage.setMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
-		MidiEvent eventOff = new MidiEvent(offShortMessage, 160);
-		track.add(eventOff);
-		
-		Sequencer sequencer = MidiSystem.getSequencer();
-		Synthesizer synthesizer = MidiSystem.getSynthesizer();
-		sequencer.open();
-		synthesizer.open();
-		sequencer.getTransmitter().setReceiver(synthesizer.getReceiver());	
-		sequencer.setSequence(sequence);
-		sequencer.setTempoInBPM(120);
-		sequencer.start();
-
-		for (int i=0;sequencer.isRunning();i++) {
-			Thread.sleep(100+i%2);
-		}
-		
-		sequencer.close();
-		
-
+		Sequence sequence = JSMusic.initialize();		
+		Track track = JSMusic.createTrack(sequence, 0);
+		JSMusic.addNote(track, 60, 70, 160);
+		Sequencer sequencer = JSMusic.play(sequence);
+		JSMusic.waitForSongDone(sequencer);
+		JSMusic.close(sequencer);
 	}
 
 }
